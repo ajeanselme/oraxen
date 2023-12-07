0ec0104b898c3cd23824d481d52b8ca81a797405
Antoine Jeanselme <67123340+ajeanselme@users.noreply.github.com>
2023-12-07 14:32:32 +0100

    Custom Tags

--- core/src/main/java/io/th0rgal/oraxen/font/SATags.java
+++ core/src/main/java/io/th0rgal/oraxen/font/SATags.java
@@ -15,6 +15,7 @@ import java.util.logging.Level;
 
 public class SATags {
     private static Map<String, TextColor> COLORS = new HashMap<>();
+    private static Map<String, String[]> GRADIENTS = new HashMap<>();
 
     private static void loadConfig() {
         if(!COLORS.isEmpty()) return;
@@ -42,6 +43,33 @@ public class SATags {
                 }
             }
         }
+
+        if(config.isConfigurationSection("gradients")) {
+            ConfigurationSection gradients = config.getConfigurationSection("gradients");
+            if(gradients != null) {
+                for (String gradient : gradients.getKeys(false)) {
+                    String value = gradients.getString(gradient);
+                    if(value != null) {
+                        String[] split = value.split(",");
+                        if(split.length == 2) {
+                            GRADIENTS.put(gradient, split);
+                        } else {
+                            OraxenPlugin.get().getLogger().log(Level.SEVERE, "Invalid color value set for " + gradient);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public static TextColor getColor(String name) {
+        TextColor color = COLORS.get(name);
+        if(color == null) {
+            OraxenPlugin.get().getLogger().log(Level.SEVERE, "Color" + name + " not found");
+            return TextColor.color(254, 254, 254);
+        }
+
+        return color;
     }
 
     public static TagResolver getResolver() {
@@ -50,6 +78,12 @@ public class SATags {
         TagResolver.Builder builder = TagResolver.builder();
         for (String color : COLORS.keySet()) {
             builder.tag(color, Tag.styling(COLORS.get(color)));
+        }
+
+        for (String gradient : GRADIENTS.keySet()) {
+            TextColor firstColor = getColor(GRADIENTS.get(gradient)[0]);
+            TextColor secondColor = getColor(GRADIENTS.get(gradient)[1]);
+            builder.tag(gradient, Tag.preProcessParsed("<gradient:" + firstColor.asHexString() + ":" + secondColor.asHexString() + ">"));
         }
 
         return builder.build();
